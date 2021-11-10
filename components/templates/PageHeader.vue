832c377abb9bd2f1c95dc366d6078f3e6e254a4d
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-11-11 00:58:13 +0530

    style: pageHeader and Containers components

---
+++ components/templates/PageHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="md:tw-py-12 tw-px-4 tw-py-6 tw-mb-6 tw-text-center">
+    <h1 class="md:tw-text-6xl tw-bg-clip-text tw-bg-gradient-to-br tw-from-pink-500 tw-to-pink-800 tw-py-2 tw-text-4xl tw-font-semibold tw-leading-10 tw-text-transparent">
+      <slot name="title">
+        Default title
+      </slot>
+    </h1>
+    <p class="md:tw-text-2xl tw-mt-4 tw-text-xl">
+      <slot name="description">
+        Default description
+      </slot>
+    </p>
+  </header>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ components/templates/PageSectionContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <section
+    class="md:py-12 py-6"
+    :class="SectionClass"
+  >
+    <div
+      class="md:px-8 container max-w-screen-xl px-4 mx-auto"
+      :class="DivContainerClass"
+      
+    >
+      <slot></slot>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    SectionClass: String,
+    DivContainerClass: String,
+  },
+};
+</script>
